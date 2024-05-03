8a6d7c21c2dd52d3c7e50ef7b1afc902ac770d97
Desire-2 <bikorimanadesire5@yahoo.com>
2024-05-03 23:57:56 +0200

    Debug

--- app.py
+++ app.py
@@ -548,4 +548,4 @@ def faq():
     return render_template('faq.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=True)
